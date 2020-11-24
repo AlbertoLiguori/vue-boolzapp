ede1d820489285e3a8a2584b3faa5a2761465ef3
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-24 16:47:22 +0100

    adds a computed function and removes a repetition in data

--- script.js
+++ script.js
@@ -8,64 +8,61 @@ let myApp = new Vue({
 
     contactsArray:addressBook,
 
-    filteredArray:addressBook,
-
     actualContact:addressBook[0],
 
     textContent:"",
 
+    searchEl:"",
   },
 
   computed:{
+    filteredArray: function() {
+      searchInput = this.searchEl;
+      return this.contactsArray.filter(function(element){
+        return element.name.toLowerCase().includes(searchInput.toLowerCase())
+      });
+
+    },
 
   },
-
 
   methods:{
 
     getAnElementByIndex:function(index){
       let d = new Date();
-      this.actualContact = this.filteredArray[index];
+      this.actualContact = this.contactsArray[index];
       this.actualContact.lastAccess= " "+ String(d.getHours()) + " : " + String(d.getMinutes())
     },
 
-    getFilteredElements:function(){
-      let searchEl = String(event.target.value)
-      this.filteredArray = this.contactsArray.filter(function(element){
-        return element.name.toLowerCase().includes(searchEl.toLowerCase())
-      });
+    getSearchEl:function(){
+      this.searchEl = event.target.value
     },
 
     getMessage: function(){
 
       let contact = this.actualContact;
-
       let d = new Date();
-
-      let newObjectMessage =
-      {
-        state:"sent",
-        textContent:event.target.value,
-        date:d.getDate().toString() +"/" +(d.getMonth()+1).toString()+"/" +d.getFullYear() + "\xa0\xa0" +d.getHours().toString() + " : " + d.getMinutes().toString(),
+      let newObjectMessage = {
+                                state:"sent",
+                                textContent:event.target.value,
+                                date:d.getDate().toString() +"/" +(d.getMonth()+1).toString()+"/" +d.getFullYear() + "\xa0\xa0" +d.getHours().toString() + " : " + d.getMinutes().toString(),
 
       };
 
-      let newReceivedObjectMessage =
-      {
-        state:"received",
-        textContent:"Io sono Groot!",
-        date:d.getDate().toString() +"/" +(d.getMonth()+1).toString()+"/" +d.getFullYear() + "\xa0\xa0" +d.getHours().toString() + " : " + d.getMinutes().toString(),
+      let newReceivedObjectMessage = {
+                                        state:"received",
+                                        textContent:"Io sono Groot!",
+                                        date:d.getDate().toString() +"/" +(d.getMonth()+1).toString()+"/" +d.getFullYear() + "\xa0\xa0" +d.getHours().toString() + " : " + d.getMinutes().toString(),
 
       };
 
       contact.messages.push(newObjectMessage)
-
       event.target.value=""
 
       setTimeout(function (){
         contact.messages.push(newReceivedObjectMessage)
 
-      },2000)
+      },1200)
     }
 
   }
